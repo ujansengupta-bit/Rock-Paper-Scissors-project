b063b556b8372021a78ba5288d72779056e9e33b
Ujan Sengupta <ujansenguptaofficial@gmail.com>
2025-11-28 12:40:43 +0530

    Js logic for the game done

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
 let humanScore = 0;
 let computerScore = 0;
 
+const results = document.querySelector("#results");
+const score   = document.querySelector("#score");
+
+const button  = document.querySelectorAll(".playButton");
+
+const reset   = document.querySelector("#reset");
+
+for(let i =0; i<button.length; i++){
+        button[i].addEventListener("click", event =>{
+            playRound(button[i].id, getComputerChoice());
+        });
+}
+
+reset.addEventListener("click", resetgame);
+
+function resetgame(){
+    humanScore = 0;
+    computerScore = 0;
+
+    results.textContent = "Choose Rock, Paper, or Scissors to start a new game!";
+    score.textContent = "YOU: 0  Computer: 0";
+
+    unpause();
+}
+
+
 function getComputerChoice() {
     const r = Math.random();
     if (r <= 1/3) return "rock";
     if (r <= 2/3) return "paper";
     return "scissors";
 }
-function getHumanChoice() {
-    let choice = prompt("Choose Rock, Paper, or Scissors:");
-    if (!choice) return null;
-
-    choice = choice.toLowerCase();
-    const valid = ["rock", "paper", "scissors"];
-
-    if (!valid.includes(choice)) {
-        console.log("Invalid input. Type only Rock, Paper, or Scissors.");
-        return null;
-    }
-
-    return choice;
-}
 
 function playRound(humanChoice, computerChoice) {
-    if (!humanChoice) {
-        console.log("Invalid input.");
+    if(humanScore >= 5 || computerScore >= 5){
         return;
     }
 
-    console.log("You chose:", humanChoice);
-    console.log("Computer chose:", computerChoice);
+    let roundResult = "";
 
     if (humanChoice === computerChoice) {
-        console.log("Draw!");
+        roundResult = 'DRAW!!! Both choose: '  + humanChoice;
         return;
     }
 
@@ -42,22 +52,33 @@ function playRound(humanChoice, computerChoice) {
         (humanChoice === "scissors" && computerChoice === "rock");
 
     if (youLose) {
-        console.log(`You lose!`);
+        roundResult = 'You LOSE!!! Computer choose: ' + computerChoice
         computerScore++;
     } else {
-        console.log(`You win!`);
+        roundResult = 'You WIN!!! Computer choose: ' + computerChoice
         humanScore++;
     }
-    console.log("Your Score:", humanScore, "Computer Score:", computerScore);
+    results.textContent = roundResult;
+    score.textContent   = 'YOU: ' + humanScore + '  Computer: ' + computerScore
+
+    if(humanScore === 5) {
+        results.textContent += "   GAME OVER: YOU WON!!!!"
+        pause();
+    }
+    if(computerScore === 5) {
+        results.textContent += "   GAME OVER COMPUTER WON!!!!"
+        pause();
+    }
 }
 
-while (true) {
-    const human = getHumanChoice();
-    if (human === null) {
-        console.log("Game ended.");
-        break;
+function pause(){
+    for(let i=0; i<button.length; i++){
+        button[i].disabled = true;
     }
+}
 
-    const computer = getComputerChoice();
-    playRound(human, computer);
+function unpause(){
+    for(let i=0; i<button.length; i++){
+        button[i].disabled = false;
+    }
 }
